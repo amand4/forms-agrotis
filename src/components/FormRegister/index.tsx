1d870cacad63378a4617856a57d92b270f03056b
amand4 <amandakarolinecs@gmail.com>
2024-04-26 13:25:26 -0300

    feat: added initial structure of the header and form component

---
+++ src/components/FormRegister/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+
+const FormRegister = () => {
+  return (
+    <Box>
+      Formulario
+    </Box>
+
+  );
+};
+
+export default FormRegister;
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,30 @@
+import { AppBar, Toolbar } from '@mui/material';
+
+import logo from '../../assets/images/logo-agrotis.png';
+
+const Header = () => {
+  return (
+    <AppBar
+      style={{
+        backgroundColor: '#FFFFFF',
+        width: '100%',
+        height: '39.1px',
+        gap: '0px',
+        opacity: '0.8',
+        boxShadow: '0px 1px 3px 0px #00000042',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Toolbar>
+        <img
+          src={logo}
+          alt="Logo Agrotis"
+        />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
