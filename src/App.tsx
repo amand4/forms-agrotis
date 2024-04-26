72f5373cd540864345b863c97596a712bce67ca4
amand4 <amandakarolinecs@gmail.com>
2024-04-26 13:24:19 -0300

    feat: added base structure

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FormRegister from './components/FormRegister'
+import Header from './components/Header'
+import { Box } from '@mui/material'
+
+function App() {
+
+  return (
+    <Box>
+      <Header />
+      <FormRegister />
+    </Box>
+  )
+}
+
+export default App
