7e4b77b2cf376ddbc52ffd65c3a3575489f0e8a1
amand4 <amandakarolinecs@gmail.com>
2024-04-29 09:17:55 -0300

    refactor: style refactoring

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,17 @@
-import { AppBar, Toolbar } from '@mui/material';
-
+import { AppBar } from '@mui/material';
 import logo from '../../assets/images/logo-agrotis.png';
+import { containerHeaderStyles } from './styles';
 
 const Header = () => {
   return (
     <AppBar
       position="static"
-      style={{
-        backgroundColor: '#FFFFFF',
-        width: '100%',
-        height: '40px',
-        gap: '0px',
-        opacity: '0.8',
-        boxShadow: '0px 1px 3px 0px #00000042',
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center"
-      }}
+      style={{ ...containerHeaderStyles }}
     >
-      <Toolbar>
-        <img
-          src={logo}
-          alt="Logo Agrotis"
-        />
-      </Toolbar>
+      <img
+        src={logo}
+        alt="Logo Agrotis"
+      />
     </AppBar>
   );
 };
